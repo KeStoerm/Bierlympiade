69dd211ce9955d16f649641818faffaf2b191fcb
Kevin Störmer <kevin.stoermer@web.de>
2021-08-03 16:26:10 +0200

    added new scripts

--- src/views/StartView.tsx
+++ src/views/StartView.tsx
@@ -5,7 +5,7 @@ export const StartView = (): JSX.Element => {
     <div className="beerLogoWrapper w-32 h-32 mb-4">
       <BeerLogo />
     </div>
-    <h2 className="text-accent text-3xl mb-12 headline">BEERlympiade</h2>
+    <h2 className="text-accent text-3xl mb-12 headline">BIERlympiade</h2>
     <button className="button w-full h-12 bg-accent text-secondary mb-2">Neue Beerlympiade anlegen</button>
     <button className="button w-full h-12 bg-accent text-secondary mb-2">tbd</button>
     <button className="button w-full h-12 bg-accent text-secondary mb-2">tbd</button>
