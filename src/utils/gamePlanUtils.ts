140cc286e3913112520db27ac195df89376eac59
Kevin Störmer <kevin.stoermer@web.de>
2021-11-29 17:01:55 +0100

    add first firebase connection

--- src/utils/gamePlanUtils.ts
+++ src/utils/gamePlanUtils.ts
@@ -4,13 +4,18 @@ import { FilledGameDay, GamePlan, PointedGame } from '../types/gameTypes';
 export const findCurrentGameDayIndex = (gamePlan: GamePlan): number =>
   findIndex(gamePlan?.gameDays, (gameDay) => !!find(gameDay.games, game => !game.winnerTeam))
 
-export const findCurrentGameIndexInGameDay = (gamePlan: GamePlan, gameDayIndex: number): number =>
-  findIndex(gamePlan.gameDays[gameDayIndex].games, game => !game.winnerTeam)
+export const findCurrentGameIndexInGameDay = (gamePlan: GamePlan, gameDayIndex: number): number => {
+  return findIndex(gamePlan.gameDays[gameDayIndex].games, game => !game.winnerTeam)
+}
+
+export const isGamePlanFinished = (gamePlan: GamePlan) => 
+  findCurrentGameDayIndex(gamePlan) === -1
 
 export const findCurrentGameDay = (gamePlan: GamePlan): FilledGameDay => gamePlan.gameDays[findCurrentGameDayIndex(gamePlan)];
 
 export const findCurrentGame = (gamePlan: GamePlan): PointedGame => {
   const gameDayIndex = findCurrentGameDayIndex(gamePlan);
+  const currentGameIndex = findCurrentGameIndexInGameDay(gamePlan, gameDayIndex);
 
-  return gamePlan.gameDays[gameDayIndex].games[findCurrentGameIndexInGameDay(gamePlan, gameDayIndex)];
+  return gamePlan.gameDays[gameDayIndex].games[currentGameIndex];
 }
--- src/views/EventOverView.tsx
+++ src/views/EventOverView.tsx
@@ -1,9 +1,10 @@
 import { map } from 'lodash';
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+import { saveFinishedGame } from '../controller/gameController';
 import { getAllGamePlans } from '../database/gamePlanDb';
 import { GamePlan, WithId } from '../types/gameTypes'
-import { findCurrentGame, findCurrentGameDay } from '../utils/gamePlanUtils';
+import { findCurrentGame, findCurrentGameDay, isGamePlanFinished } from '../utils/gamePlanUtils';
 
 export const EventOverView = (): JSX.Element => {
   const [gamePlans, setGamePlans] = useState<Array<GamePlan & WithId>>();
@@ -17,12 +18,14 @@ export const EventOverView = (): JSX.Element => {
 
   return <div>
     {map(gamePlans, (gamePlan) => {
-    const currentGameDay = findCurrentGameDay(gamePlan);
-    const currentGame = findCurrentGame(gamePlan);
+    const isPlanFinished = isGamePlanFinished(gamePlan);
+    const currentGameDayName = isPlanFinished ? "" : findCurrentGameDay(gamePlan).name;
+    const currentGameName = isPlanFinished ? "" : findCurrentGame(gamePlan).name;
     return <Link to={`/game/${gamePlan.id}`} key={gamePlan.alcBonus} className="gameCard w-full block p-4 bg-accent mb-4">
       <div className="cardHead flex justify-between w-full pb-4 headline">
-        <h4 className="gameDay">{currentGameDay.name}</h4>
-        <div className="game">{currentGame.name}</div>
+        <h4 className="gameDay">{currentGameDayName}</h4>
+        <div className="game">{currentGameName}</div>
+        <button onClick={() => saveFinishedGame(gamePlan)}>speichern</button>
       </div>
       <div className="cardBody pl-4">
       </div>
