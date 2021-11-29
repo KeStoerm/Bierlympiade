be708957d54b9467a13eb44c08ed982893a59dfe
Kevin Störmer <kevin.stoermer@web.de>
2021-11-29 17:31:04 +0100

    pushing first buggy auth

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,42 @@ import { StartView } from './views/StartView';
 import { GameCreationView } from './views/gameCreationView/GameCreationView';
 import { annealGamePlan } from './utils/gamePlanAnnealingUtils';
 import { gameDays, games, pointBuckets } from './data/gameData';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { FirebaseOptions, initializeApp } from '@firebase/app';
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyB9BeaO_r0VDt6YO3trOTkVBLuwhsHMg9I",
+  databaseURL: "https://bierlympiade-70679-default-rtdb.europe-west1.firebasedatabase.app/",
+  authDomain: "bierlympiade-70679.web.app",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+const auth = getAuth();
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    console.log("worked")
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result)!;
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+    // ...
+  }).catch((error) => {
+    console.log(error)
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 
 register();
 
--- src/controller/gameController.ts
+++ src/controller/gameController.ts
@@ -1,18 +1,9 @@
-import { initializeApp } from 'firebase/app';
 import { getDatabase, push, ref } from "firebase/database";
 import { omit } from 'lodash';
 import { GamePlan } from '../types/gameTypes';
 
-const firebaseConfig = {
-  databaseURL: "https://bierlympiade-70679-default-rtdb.europe-west1.firebasedatabase.app/",
-};
-
-const app = initializeApp(firebaseConfig);
-
-// Get a reference to the database service
-const remoteDatabase = getDatabase(app)
-
 export const saveFinishedGame = (finishedGame: GamePlan) => {
+  const remoteDatabase = getDatabase();
   const validFinishedGame = omit(finishedGame, "id")  
   console.log(validFinishedGame);
   push(ref(remoteDatabase, "games"), validFinishedGame);
